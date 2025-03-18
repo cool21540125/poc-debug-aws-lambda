580c8eb1e3f71680116c2640109bc7157701b414
Tony <tony@weibyapps.com>
2025-03-18 15:21:16 +0800

    Infrastructure Composer & lauhcn.json debug ApiGateway

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
+# Note
 
-- https://www.youtube.com/watch?v=RDzFA8ssAws&list=PLHsofw25VEfR3Sc1Hi5i7WOvvrDGcnAbu
+- 2025/03/18
 
-
-# debug Lambda Function locally
-
-- directly invoke
-- CloudFormation Template invoke debug
-- Api Gateway invoke debug
-
+- https://www.youtube.com/watch?v=RDzFA8ssAws&list=PLHsofw25VEfR3Sc1Hi5i7WOvvrDGcnAbu&index=1
+  - 只有講一個重點:
+    - 在本地使用 VSCode debug Lambda 的時候, 如何設定 VSCode 的 launch.json
+      - payload 可使用 payload.json 或 payload.path
+      - 此為 VSCode debug Lambda `direct invoke`
+- https://www.youtube.com/watch?v=pHFS-YrX_KQ&list=PLHsofw25VEfR3Sc1Hi5i7WOvvrDGcnAbu&index=2
+  - 講兩個重點:
+    - 1. 使用 `AWS Infrastructure Composer` 使用 UI 的方式拉出一個架構 (生成 CloudFormation Template)
+      - template 會同步到 local filesystem 的 template.yaml
+      - 並且自動生成必要的檔案及資料夾 Cool~
+      - 操作方式: Create project -> Menu -> Local sync activated -> (然後選擇本地沒有 CF Template 的資料夾), 同意以後, 就可以開始拉 UI (同時異動 template)
+    - 2. 使用 VSCode launch.json 設定使用 invokeTarget.target: template 的方式做 debug
+      - 此為 VSCode debug Lambda `Template-based Lambda invoke`
+- https://www.youtube.com/watch?v=y3ZfoCZ_yzg&list=PLHsofw25VEfR3Sc1Hi5i7WOvvrDGcnAbu&index=3
+  - 有幾個重點:
+    - 1. 使用 `AWS Infrastructure Composer` 開啟一個已存在的 CloudFormation project
+      - 操作方式: Create project -> Menu -> Open project folder
 
 # 缺少了
 
--- src/login/app.py
+++ src/login/app.py
@@ -1,9 +1,58 @@
+import json
+
 
 def lambda_handler(event, context):
+    """
+    並不是一個很好的整合範例= =
+    總之只是要用來呈現, launch.json 在:
+    - direct invoke
+    - template-based invoke
+    - api gateway invoke
+
+    的情境之下的 event 內容
+    """
     print(f"event {event}")
     print(f"context: {context}")
 
-    response = {
-        'statusCode': 200,
-        'body': 'hello from lambda'
-    }
+    success, username = login(event)
+
+    if success:
+        # (模擬) API Gateway 的 debug 情境
+        print(f"username: {username} 成功從 ApiGateway 登入")
+        response = {"statusCode": 200, "body": "OK -- ApiGateway to Lambda Login"}
+        return response
+    else:
+        username = event.get("username", None)
+        password = event.get("password", None)
+        print(f"=== username: {username}, password: {password} ===")
+
+        response = {"statusCode": 200, "body": "hello from lambda"}
+
+        return response
+
+
+def login(event: dict):
+    success = False
+
+    u_name = get_value_from_event(event, "username")
+    passwd = get_value_from_event(event, "password")
+
+    # verify u_name && passwd
+    if u_name == "TonyChou44" and passwd == "1234z":
+        success = True
+
+    return success, u_name
+
+
+def get_value_from_event(payload: dict, key: str):
+    if "body" in payload:
+        payload = payload["body"]
+
+    if type(payload) == str:
+        try:
+            payload = json.loads(payload)
+        except Exception as err:
+            print(str(err))
+
+    value = payload.get(key, None)
+    return value
